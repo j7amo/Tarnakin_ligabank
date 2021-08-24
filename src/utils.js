2dd27744e95eb2967f205eacbc1d94579bbdf1ee
j7amo <roman.tarnakin@gmail.com>
2021-08-24 10:50:00 +0300

    Уберёт магическое значение из utils.js

--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,5 @@
-const round = (num) => Math.round((num + Number.EPSILON) * 10000) / 10000;
+const ROUNDING_LIMIT = 10000;
+
+const round = (num) => Math.round((num + Number.EPSILON) * ROUNDING_LIMIT) / ROUNDING_LIMIT;
 
 export { round };
